581b8b86f9919ede73949b152880fb8a20bbfece
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2026-01-20 19:34:44 +0000

    AI: join orders and customers to find top 10 customers by spend

--- pipelines/top10.py
+++ pipelines/top10.py
@@ -1,13 +1,6 @@
 with DAG():
-    orders = Task(
-        task_id = "orders", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "orders", "sourceType" : "Table", "sourceName" : "danyelle.demo", "alias" : ""}
-    )
-    customers = Task(
-        task_id = "customers", 
-        component = "Dataset", 
-        writeOptions = {"writeMode" : "overwrite"}, 
-        table = {"name" : "customers", "sourceType" : "Table", "sourceName" : "danyelle.demo", "alias" : ""}
+    top10__top_10_customers_by_spend = Task(
+        task_id = "top10__top_10_customers_by_spend", 
+        component = "Model", 
+        modelName = "top10__top_10_customers_by_spend"
     )
