93714f26ca37ccece03dfe603b47bc4cdfd40668
brooks.danyelle@prophecy.io <brooks.danyelle@prophecy.io>
2026-01-20 22:29:52 +0000

    Remove `top10__top_10_customers_by_spend` transformation and update `top10` pipeline to use source datasets only

--- pipelines/top10.py
+++ pipelines/top10.py
@@ -1,6 +1,13 @@
 with DAG():
-    top10__top_10_customers_by_spend = Task(
-        task_id = "top10__top_10_customers_by_spend", 
-        component = "Model", 
-        modelName = "top10__top_10_customers_by_spend"
+    customers = Task(
+        task_id = "customers", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "customers", "sourceType" : "Table", "sourceName" : "danyelle.demo", "alias" : ""}
+    )
+    orders = Task(
+        task_id = "orders", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "orders", "sourceType" : "Table", "sourceName" : "danyelle.demo", "alias" : ""}
     )
